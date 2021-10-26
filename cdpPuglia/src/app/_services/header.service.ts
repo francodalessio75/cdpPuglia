60fc8a3eba8b3c745ec6b6341803359bc4e99672
Franco DAlessio <francodalessio@tailorinformatica.it>
2021-10-26 12:04:56 +0200

    Introdotte Titolo e Descrizione in intestazione

---
+++ cdpPuglia/src/app/_services/header.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeaderService {
+  title = '';
+  description = '';
+  
+  private currentTitleDescription = new ReplaySubject<{title:string, description:string}>(1);
+  titleDescription$ = this.currentTitleDescription.asObservable();
+
+  setCurrentTitleDescription( title:string, description:string ){
+    this.currentTitleDescription.next({title,description});
+  }
+
+  constructor() { }
+}
