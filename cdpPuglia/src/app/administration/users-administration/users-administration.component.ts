d872b325c09d9eb9ad397fbc8964504529b84cbd
erica271 <ericaeli27@yahoo.it>
2021-12-15 14:39:06 +0100

    commit user-sadministration

--- cdpPuglia/src/app/administration/users-administration/users-administration.component.ts
+++ cdpPuglia/src/app/administration/users-administration/users-administration.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { MatAccordion } from '@angular/material/expansion';
 import { ToastrService } from 'ngx-toastr';
 import { HasSpinner } from 'src/app/interfaces/hasSpinner';
 import { User } from 'src/app/_models/user';
 import { SpinnerService } from 'src/app/_services/spinner.service';
 import { UsersAdministrationService } from 'src/app/_services/users-admin.service';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
 @Component({
   selector: 'app-users-administration',
@@ -22,7 +24,8 @@ export class UsersAdministrationComponent implements OnInit, HasSpinner {
   constructor(
     private spinnerService:SpinnerService,
     private usersAdministrationService:UsersAdministrationService,
-    private toastrService:ToastrService
+    private toastrService:ToastrService,
+    private dialog: MatDialog
   ) 
   {
     this.spinnerService.loading$.subscribe(
@@ -36,6 +39,21 @@ export class UsersAdministrationComponent implements OnInit, HasSpinner {
 
   ngOnInit(): void {
     this.usersAdministrationService.getUsers();
+  }
+  openUserDetails(){
+    // this.usersAdministrationService.setUser(userId);
+
+    const dialogRef = this.dialog.open(UserDetailsComponent, {
+      // width: '250px',
+      panelClass: 'trend-dialog',
+      height: '40%',
+      width: '60%',
+      disableClose: true,
+      hasBackdrop: true
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
   }
 
 }
